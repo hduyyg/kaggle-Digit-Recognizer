05e27fb0cdc82f1c55835b51625f401101f868bc
hduyyg <2632158294@qq.com>
2018-02-14 11:42:38 +0800

    train for knn,make sure the value of k in knn

--- train/knn.py
+++ train/knn.py
@@ -7,7 +7,7 @@ from sklearn import neighbors, cross_validation
 logging.basicConfig(level=logging.INFO,
         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
         datefmt='%a, %d %b %Y %H:%M:%S',
-        filename='digit_recognize.log',
+        filename='digit_recognize_knn.log',
         filemode='w+')
 
 
@@ -16,13 +16,13 @@ def main():
     datas = infos.values[0:,1:]
     labels = infos.values[0:,0]
     
-    file = open('log.txt', 'w+')
-    for n_neighbors in range(1, 21):
+    for n_neighbors in range(6, 11):
         for cv in [5, 10, 20]:
+            logging.info('start n_neighbors={} cv={}'.format(n_neighbors, cv))
             clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors)
             scores = cross_validation.cross_val_score(clf, datas, labels, cv=cv, scoring='accuracy')
-            file.write('k={} cv={} score={}'.format(n_neighbors, cv, scores.mean()))
-    file.close()
+            logging.info('score={}'.format(scores.mean()))
+
 
 if __name__ == '__main__':
     main()
