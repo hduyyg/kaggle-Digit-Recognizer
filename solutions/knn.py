dfd180acfd2237b6b62552087fb50f2c05457672
hduyyg <2632158294@qq.com>
2018-03-08 22:39:37 +0800

    delete some invalid functions in solutions/knn.py

--- solutions/knn.py
+++ solutions/knn.py
@@ -6,25 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 import functions
 
 
-def get_knn_classify(trainData, trainLabel):
-    logging.info('start get knnClf by train.')
-    knnClf = KNeighborsClassifier(n_neighbors=3)
-    knnClf.fit(trainData, np.ravel(trainLabel))  # ravel 
-    return knnClf
-
-
-def predict(train_data, train_label, test_data):
-    knn_clf = get_knn_classify(train_data, train_label)
-    joblib.dump(knn_clf, 'data/knn.m')
-    logging.info('start predict the result.')
-    test_label = knn_clf.predict(test_data)
-    functions.save_result(test_label, 'result_knn')
-
-
-def test_args(train_data, train_label):
-    pass
-
-
 def get_train_model(data, label, path_prefix, flags):
     if flags['command'] != 'get_best_train_model':
         model = KNeighborsClassifier(n_neighbors=3)
