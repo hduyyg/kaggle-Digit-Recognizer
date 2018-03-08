92a0f57b1a1daacbc324a694c9cbc715a2f9f40b
hduyyg <2632158294@qq.com>
2018-03-08 22:34:51 +0800

    add get_train_model(data, label, path_prefix, flags) in solutions/knn.py

--- solutions/knn.py
+++ solutions/knn.py
@@ -25,13 +25,25 @@ def test_args(train_data, train_label):
     pass
 
 
+def get_train_model(data, label, path_prefix, flags):
+    if flags['command'] != 'get_best_train_model':
+        model = KNeighborsClassifier(n_neighbors=3)
+        model.fit(data, np.ravel(label))
+        joblib.dump(model, path_prefix + '_knn_default.m')
+        return
+
+    if flags['command'] == 'get_best_train_model':
+        # TODO:Implement the function-get_best_train_model in knn
+        pass
+
+
 def main(flags):
     if flags['train_data'] is not None:
         logging.info('get the train model by {}'.format(flags['train_data']))
         path_prefix = 'data/' + flags['train_data']
         train_data = np.load(path_prefix + '.npy')
         train_label = np.load('data/train_label.npy')
-        get_train_model(train_data, train_label, path_prefix)
+        get_train_model(train_data, train_label, path_prefix, flags)
     
     if flags['test_data'] is not None:
         if flags['train_model'] is None:
@@ -40,4 +52,7 @@ def main(flags):
         model = joblib.load('data/' + flags['train_model'])
         result_name = 'data/' + flags['train_model'] + '_result'
         test_data = np.load('data/' + flags['test_data'] + '.npy')
-        predict(test_data, model, result_name)
+        predict(test_data, model, result_name, flags)
+
+# command:
+# get_best_train_model
